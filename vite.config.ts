3460998a4640bafb2bd9570b4fb20805026593f7
zhilutianji <starmars_lu@163.com>
2023-11-08 15:02:32 +0800

    imagemin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteImagemin from 'vite-plugin-imagemin'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ registerType: 'autoUpdate' }),
     viteImagemin({
       gifsicle: {
         // gif图片压缩
