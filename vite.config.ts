ddd671a7ac0e3bc6cae5849ec64cc86ba66e5d46
zhilutianji <starmars_lu@163.com>
2023-11-08 15:52:40 +0800

    fix: remove pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteImagemin from 'vite-plugin-imagemin'
-import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' }),
     viteImagemin({
       gifsicle: {
         // gif图片压缩
